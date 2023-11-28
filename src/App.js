37359fa35d630ffaa7f50b82c128636b077df9b6
Perishte <javascript_9595@bk.ru>
2023-11-28 07:35:56 +0500

    React 6th task Routers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {Link, Outlet} from 'react-router-dom';
+import Home from "./components/pages/Home";
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.mainWrap}>
+      <ul className={styles.menuList}>
+        <li className={styles.list}>
+          <Link to={'/home'} className={styles.link}>Home Page</Link>
+        </li>
+        <li className={styles.list}>
+          <Link to={'/about-us'} className={styles.link}>About Us Page</Link>
+        </li>
+        <li>
+          <Link to={'/sign-in'} className={styles.link}>Sign In Page</Link>
+        </li>
+      </ul>
+      <Home />
     </div>
   );
 }
